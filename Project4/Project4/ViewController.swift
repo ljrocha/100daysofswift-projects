96e3b2d3eb10960510f60f963ebd6941a3ee7556
ljrocha <ljrocha@users.noreply.github.com>
2019-03-22 22:15:01 -0700

    Day 26 Project 4 Challenges complete

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -12,7 +12,11 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
     var progressView: UIProgressView!
-    var websites = ["apple.com", "hackingwithswift.com"]
+    var websites: [String]!
+    var websiteSelected = 0
+    
+    var backButtonItem: UIBarButtonItem!
+    var forwardButtonItem: UIBarButtonItem!
     
     override func loadView() {
         webView = WKWebView()
@@ -23,25 +27,34 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.largeTitleDisplayMode = .never
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
-        
-        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
         
-        toolbarItems = [progressButton, spacer, refresh]
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        backButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        backButtonItem.isEnabled = false
+        forwardButtonItem = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
+        forwardButtonItem.isEnabled = false
+        
+        toolbarItems = [progressButton, spacer, backButtonItem, spacer, forwardButtonItem, spacer, refresh]
         navigationController?.isToolbarHidden = false
         
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
-        let url = URL(string: "https://" + websites[0])!
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack), options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward), options: .new, context: nil)
+        
+        let url = URL(string: "https://" + websites[websiteSelected])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
-
+    
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open...", message: nil, preferredStyle: .actionSheet)
         
@@ -74,12 +87,20 @@ class ViewController: UIViewController, WKNavigationDelegate {
             }
         }
         
+        let ac = UIAlertController(title: "Blocked", message: "The website \(navigationAction.request.url!) is blocked", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
         decisionHandler(.cancel)
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "estimatedProgress" {
             progressView.progress = Float(webView.estimatedProgress)
+        } else if keyPath == "canGoBack" {
+            backButtonItem.isEnabled = webView.canGoBack
+        } else if keyPath == "canGoForward" {
+            forwardButtonItem.isEnabled = webView.canGoForward
         }
     }
 }
