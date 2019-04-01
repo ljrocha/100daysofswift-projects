16569f84f26df624511c1e1f494a0ec30da46aac
ljrocha <ljrocha@users.noreply.github.com>
2019-03-31 22:19:17 -0700

    Day 35 Project 7 Challenges complete

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(creditsTapped))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterTapped))
         
         let urlString: String
         if navigationController?.tabBarItem.tag == 0 {
@@ -43,17 +47,49 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = jsonPetitions.results
             tableView.reloadData()
         }
     }
     
+    @objc func creditsTapped() {
+        let ac = UIAlertController(title: "This data comes from the We The People API of the Whitehouse", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func filterTapped() {
+        let ac = UIAlertController(title: "Enter text to filter by...", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let filterAction = UIAlertAction(title: "Filter", style: .default) { [weak self, weak ac] _ in
+            guard let text = ac?.textFields?[0].text else { return }
+            self?.filter(by: text)
+        }
+        ac.addAction(filterAction)
+        
+        present(ac, animated: true)
+    }
+    
+    func filter(by text: String) {
+        if text.isEmpty {
+            filteredPetitions = petitions
+        } else {
+            let lowerText = text.lowercased()
+            filteredPetitions = petitions.filter { petition in
+                return petition.title.lowercased().contains(lowerText) || petition.body.lowercased().contains(lowerText)
+            }
+        }
+        tableView.reloadData()
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -61,7 +97,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 
