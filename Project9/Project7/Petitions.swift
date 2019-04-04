d07f3a66135e42f75bfeeb12ae7b77c2e5c4f2d4
ljrocha <ljrocha@users.noreply.github.com>
2019-04-04 13:19:26 -0700

    Day 39 Project 9 Part 1 complete

---
+++ Project9/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Leandro Rocha on 3/29/19.
+//  Copyright © 2019 Leandro Rocha. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
