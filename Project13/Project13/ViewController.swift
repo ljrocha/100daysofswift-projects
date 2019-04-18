4657fd2abf108cfd44928758632cee73ec736051
ljrocha <ljrocha@users.noreply.github.com>
2019-04-17 23:10:29 -0700

    Day 52 Project 13 Part 1

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Leandro Rocha on 4/17/19.
+//  Copyright © 2019 Leandro Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+
+    @IBAction func changeFilter(_ sender: UIButton) {
+        
+    }
+    @IBAction func save(_ sender: UIButton) {
+        
+    }
+    @IBAction func intensityChanged(_ sender: UISlider) {
+        
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        currentImage = image
+    }
+    
+}
+
