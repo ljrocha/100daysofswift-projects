f4f09f68944677c9b7747f747eb1633c73453387
ljrocha <ljrocha@users.noreply.github.com>
2019-05-05 22:51:55 -0700

    Day 67 Project 19 Part 1 complete

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Leandro Rocha on 5/5/19.
+//  Copyright © 2019 Leandro Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
